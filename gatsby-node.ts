0d4f13af1b172fe586dbbeaa14f1e3fff7e1426a
Ubuntu <ubuntu@ip-172-31-39-23.sa-east-1.compute.internal>
2022-12-11 20:53:46 +0000

    Querying all countries and states and creating placeholder pages for them with the name at the top

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,1 +1,82 @@
-// use Typescript with gatsby node apis here
+import * as path from 'path'
+import { GatsbyNode } from 'gatsby'
+
+interface Places {
+  error?: any
+  data?: {
+    countries: {
+      nodes: {
+        data: {
+          Country_Name: string
+          ISO_alpha3_code: string
+        }
+      }[]
+    }
+    states: {
+      nodes: {
+        data: {
+          Name: string
+        }
+      }[]
+    }
+  }
+}
+
+export const createPages: GatsbyNode['createPages'] = async ({
+  graphql,
+  actions,
+}) => {
+  const policyPageTemplate = path.resolve('./src/templates/PolicyPage.tsx')
+
+  const places: Places = await graphql(`
+    query {
+      countries: allAirtable(
+        filter: { table: { eq: "ISO Code Look-up" } }
+        sort: { data: { Country_Name: ASC } }
+      ) {
+        nodes {
+          data {
+            ISO_alpha3_code
+            Country_Name
+          }
+        }
+      }
+      states: allAirtableIntermediateAreas(
+        filter: {
+          data: { ISO_alpha3_code__from_ISO_Code_Look_up_: { eq: "USA" } }
+        }
+        sort: { data: { Name: ASC } }
+      ) {
+        nodes {
+          data {
+            Name
+          }
+        }
+      }
+    }
+  `)
+
+  if (!places.data) throw new Error('Places query error')
+
+  places.data.countries.nodes.forEach(node => {
+    actions.createPage({
+      path: `policies/${node.data.ISO_alpha3_code}`,
+      component: policyPageTemplate,
+      context: {
+        iso3: node.data.ISO_alpha3_code,
+        name: node.data.Country_Name,
+      },
+    })
+  })
+
+  places.data.states.nodes.forEach(node => {
+    actions.createPage({
+      path: `policies/USA/${node.data.Name}`,
+      component: policyPageTemplate,
+      context: {
+        iso3: 'USA',
+        name: node.data.Name,
+      },
+    })
+  })
+}
---
+++ src/templates/PolicyPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PolicyPage = ({ data, pageContext }) => {
+  return (
+    <>
+      <h1>{pageContext.name}</h1>
+      <h2>Policy Page</h2>
+    </>
+  )
+}
+
+export default PolicyPage
