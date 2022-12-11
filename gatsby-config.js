86589e76442e781dd5fce0a76e4331f330143dab
Ubuntu <ubuntu@ip-172-31-39-23.sa-east-1.compute.internal>
2022-12-11 20:53:05 +0000

    Adding intermediate areas table using a query name alias to avoid conflict

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,17 +48,19 @@ module.exports = {
           {
             baseId: `appoXaOlIgpiHK3I2`,
             tableName: `Intermediate Area Database`,
-            tableView: `Grid view`,
+            tableView: `Ryan Grid`,
+            queryName: `IntermediateAreas`,
+            separateNodeType: true,
           },
-          {
-            baseId: `appoXaOlIgpiHK3I2`,
-            tableName: `Policy Database`,
-            tableView: `Final Database`,
-            tableLinks: [
-              `Authorizing_country_name`,
-              `Authorizing_state_province__if_applicable`,
-            ],
-          },
+          // {
+          //   baseId: `appoXaOlIgpiHK3I2`,
+          //   tableName: `Policy Database`,
+          //   tableView: `Final Database`,
+          //   tableLinks: [
+          //     `Authorizing_country_name`,
+          //     // `Authorizing_state_province__if_applicable`,
+          //   ],
+          // },
         ],
       },
     },
